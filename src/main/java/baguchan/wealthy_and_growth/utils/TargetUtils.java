de3637e2f1362b0ec05b2d7d80f5fe7ed9ea2d3b
baguchan <shutux500@gmail.com>
2023-07-12 17:02:42 +0900

    1.20.1-3.1.0

--- src/main/java/baguchan/wealthy_and_growth/utils/TargetUtils.java
+++ src/main/java/baguchan/wealthy_and_growth/utils/TargetUtils.java
@@ -8,9 +8,9 @@ public class TargetUtils {
 	public static boolean canTarget(Player player, RandomSource random, float lowerLimit){
 		final boolean[] isTarget = new boolean[1];
 		player.getCapability(WealthyAndGrowth.PLAYER_TARGET_CAPABILITY).ifPresent(cap ->{
-			if(random.nextFloat() > (1.0F - cap.getEffectiveTargetScale()) + lowerLimit){
+			if (random.nextFloat() < (1.0F - cap.getEffectiveTargetScale()) + lowerLimit) {
 				isTarget[0] = true;
-			}else {
+			} else {
 				isTarget[0] = false;
 			}
 		});
